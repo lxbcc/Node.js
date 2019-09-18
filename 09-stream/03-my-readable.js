1b5f5406e49399649a0457535cf344a55564b670
lxbcc <1667899744@qq.com>
2019-09-18 16:15:35 +0800

    c1

--- 09-stream/03-my-readable.js
+++ 09-stream/03-my-readable.js
@@ -1,21 +1,19 @@
 #!/usr/binde
 const Readable = require('stream').Readable;
 
-var c = 'a'.charCodeAt(0);
-
 var MyReadable = function() {
     Readable.call(this);
 
 };
 
 MyReadable.prototype = Readable.prototype;
+var c='a'.charCodeAt(0);
+
 
 MyReadable.prototype._read = function() {
     this.push(String.fromCharCode(c++));
       if(c>'z'.charCodeAt(0)) this.push(null);
-
 };
+module.exports=MyReadable;
 
-var rs = new MyReadable();
-rs.pipe(process.stdout);
 
---
+++ 10-file-system/01-my-cat-sync.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs=require('fs'),
+      file=process.argv[2] || __filename;
+if(fs.existsSync(file)){
+console.log(fs.readFileSync(file).toString('utf8'));
+}else{
+  console.log('%s not exists',file);
+  process.exit(1);
+}
